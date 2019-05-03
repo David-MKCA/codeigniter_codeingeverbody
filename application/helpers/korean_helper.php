40b32a68840f8c9b71e28587d697d568bae9883c
davidkim.mkca <davidkim.mkca@gmail.com>
2019-05-03 15:05:22 +0900

    KST로 표시되게 변경

--- application/helpers/korean_helper.php
+++ application/helpers/korean_helper.php
@@ -2,6 +2,10 @@
 
 if ( ! function_exists('kdate')){
   function kdate($stamp){
-    return date("o년 n월 j일, G시 i분 s초", $stamp);
+    $tmp1 = strtotime("2019-05-02 18:00:00");
+    $tmp2 = strtotime("2019-05-02 02:00:00");
+    $tmp3 = $tmp1-$tmp2; //KST = UNIX_TIMESTAMP + 16h
+    return date("o년 n월 j일, G시 i분 s초", $stamp+$tmp3);
+//    return date("o년 n월 j일, G시 i분 s초", $stamp);
   }
 }
